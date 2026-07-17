3b9f0bf77c1eed0e33b1831984cebabdc95e2e00
SR vibe <109028344+subon1234@users.noreply.github.com>
2026-07-17 16:23:23 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const searchRoute = require("./routes/search");
+require("dotenv").config();
+const playRoute = require("./routes/play");
+const app = express();
+const lyricsRoute = require("./routes/lyrics");
+const playlistRoute = require("./routes/playlist");
 
+app.use(cors());
+app.use(express.json());
+app.use("/play", playRoute);
+app.use("/lyrics", lyricsRoute);
+app.use("/playlist", playlistRoute);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    name: "Anizo API",
+    version: "1.0.0",
+    message: "Backend is running "
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Anizo API running on http://localhost:${PORT}`);
+});

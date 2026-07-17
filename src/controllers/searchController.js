665a1ac419d2b038633b5e61143c35cfe59dad10
SR vibe <109028344+subon1234@users.noreply.github.com>
2026-07-17 21:40:42 +0530

    Refactor search function to use searchSongs service

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,52 +1,30 @@
-const { getYoutube } = require("../services/youtubeService");
+const { searchSongs } = require("../services/searchService");
+const { success, error } = require("../utils/response");
 
 async function search(req, res) {
   try {
     const query = req.query.q;
 
     if (!query) {
-      return res.status(400).json({
-        success: false,
-        message: "Search query is required"
-      });
+      return error(res, "Search query is required", 400);
     }
 
-    const yt = await getYoutube();
+    const songs = await searchSongs(query);
 
-    const result = await yt.search(query, {
-      type: "video"
-    });
-
-    const songs = result.results
-      .filter(item => item.type === "Video")
-      .map(item => ({
-        videoId: item.id,
-        title: item.title?.text || item.title || "",
-        artist:
-          item.author?.name ||
-          item.authors?.[0]?.name ||
-          "Unknown",
-        duration: item.duration?.text || item.duration || "",
-        thumbnail:
-          item.thumbnails?.[0]?.url ||
-          item.thumbnail?.[0]?.url ||
-          ""
-      }));
-
-    res.json({
-      success: true,
-      total: songs.length,
-      results: songs
-    });
+    return success(
+      res,
+      {
+        total: songs.length,
+        results: songs
+      },
+      "Songs fetched successfully"
+    );
 
   } catch (err) {
-    console.error(err);
-
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+    return error(res, err.message, 500);
   }
 }
 
-module.exports = { search };
+module.exports = {
+  search
+};

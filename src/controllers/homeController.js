0c0795ea0090ec88f3345f7aa2c9794db915814b
SR vibe <109028344+subon1234@users.noreply.github.com>
2026-07-17 22:06:01 +0530

    Refactor homeController to use homeService

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,51 +1,20 @@
-const { Innertube } = require("youtubei.js");
-
-let yt;
-
-async function getYoutube() {
-  if (!yt) {
-    yt = await Innertube.create();
-  }
-  return yt;
-}
+const { getHomeData } = require("../services/homeService");
+const { success, error } = require("../utils/response");
 
 async function home(req, res) {
   try {
-    const youtube = await getYoutube();
+    const data = await getHomeData();
 
-    const trending = await youtube.search("Trending music");
-
-    const songs = (trending.results || [])
-      .filter(item => item.type === "Video")
-      .slice(0, 20)
-      .map(item => ({
-        id: item.id,
-        title: item.title?.text || "Unknown",
-        artist: item.author?.name || "Unknown",
-        thumbnail:
-          item.thumbnails?.[item.thumbnails.length - 1]?.url || "",
-        duration: item.duration?.text || ""
-      }));
-
-    res.json({
-      success: true,
-
-      featured: songs[0] || null,
-
-      sections: {
-        trending: songs.slice(0, 10),
-        quickPicks: songs.slice(10, 20),
-        newReleases: songs.slice(0, 10),
-        recommended: songs.slice(5, 15)
-      }
-    });
-
+    return success(
+      res,
+      data,
+      "Home data fetched successfully"
+    );
   } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
+    return error(res, err.message, 500);
   }
 }
 
-module.exports = { home };
+module.exports = {
+  home
+};

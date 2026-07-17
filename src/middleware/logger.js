4198cf4ad40924e4532c17b343c0780dc3973ac2
SR vibe <109028344+subon1234@users.noreply.github.com>
2026-07-17 21:33:10 +0530

    Add logger middleware for request logging

---
+++ src/middleware/logger.js
@@ -0,0 +1,11 @@
+function logger(req, res, next) {
+  const time = new Date().toISOString();
+
+  console.log(
+    `[${time}] ${req.method} ${req.originalUrl}`
+  );
+
+  next();
+}
+
+module.exports = logger;
